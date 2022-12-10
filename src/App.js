ff10a4ee94a7451ffd25cee9b5cf6a2b9d239044
AdityaBr11 <adityaanandpurnea1@gmail.com>
2022-12-10 13:01:08 +0530

    checking

--- src/App.js
+++ src/App.js
@@ -1,15 +1,49 @@
-import {useState} from 'react'
+import { Box, Button, Heading, Image } from '@chakra-ui/react';
 import './App.css';
 
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const getData=(da)=>{
+  return axios.get(`https://api.github.com/search/repositories?q=stars:%3E1+language:${da}`)
+}
+
 function App() {
-  const [count,setCount]=useState(0)
+  const [data, setdata] = useState([])
+  const handleGet=async (args="all")=>{
+    getData("all").then(d=>setdata(d.data.items))
+  }
+  useEffect(() => {
+    handleGet()
+  },[])
+  console.log(data)
   return (
-    <div className='App'>
-      <h1>{count}</h1>
-      <div>
-         <button onClick={()=>setCount(count-1)}>DEC</button>
-         <button onClick={()=>setCount(count+1)}>INC</button>
-      </div>
+    <div>
+      <Box>
+        <Button >ALL</Button>
+        <Button>Python</Button>
+        <Button >CSS</Button>
+        <Button >Typescript</Button>
+        <Button >JavaScipt</Button>
+      </Box>
+        {data.map((item)=>(
+          <div key={item.id}>
+            <Box ml="40px">
+            <Image src={item.owner.avatar_url} width="100px" height="100px"/>
+            <Heading>
+              {item.name}
+              </Heading>
+              <Heading>
+              {item.language}
+              </Heading>
+              <Heading>
+              {item.forks}_forks
+              {`     `}
+              {item.watchers_count} stars
+              </Heading>
+            </Box>
+          </div>
+        ))}
     </div>
   );
 }
